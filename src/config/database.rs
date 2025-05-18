87b2e59045b608bf7c54d0c3ca95ac067747bb41
vickyone0 <vigneshs0.618@gmail.com>
2025-05-18 09:32:32 -0400

    database config

---
+++ src/config/database.rs
@@ -0,0 +1,38 @@
+
+use serde::Deserialize;
+use sqlx::postgres::{PgPool, PgPoolOptions};
+
+
+#[derive(Debug, Deserialize)]
+pub struct DatabaseConfig{
+    pub host: String,
+    pub port: u16,
+    pub user: String,
+    pub password: String,
+    pub dbname: String
+
+}
+
+impl DatabaseConfig{
+    pub fn from_env() -> Result<Self, config::ConfigError>{
+        config::Config::builder()
+        .add_source(config::Enviroment::default())
+        .build()?
+        .try_deserialize()
+    }
+
+    pub fn connection_string(&self) -> String{
+        format!(
+            "postgres://{}:{}@{}:{}/{}",
+            self.user, self.password, self.host, self.port, self.dbname
+        )
+    }
+
+    pub async fn create_pool(&self) -> Result<PgPool, sqlx::Error> {
+        PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&self.connection_string())
+        .await
+
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,33 @@
+
+use actix_web::{middleware, web, App, HttpServer};
+use sqlx::postgres::PgPool;
+
+use config::DatabaseConfig;
+
+mod config;
+
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()>{
+
+    //load configartion 
+    let db_config = DatabaseConfig::from_env().expect("Failed to load config");
+    
+    //setup db connectionn pool
+    let db_pool=  PgPool::connect(&db_config.connection_string())
+    .await
+    .expect("Failed to create pool");
+
+    //run migration
+    sqlx::migrate().run(&db_pool).await.expect("Failed to run migration");
+
+    HttpServer::new(move || {
+        App::new()
+        .app_data(web::Data::new(db_pool.clone()))
+        .configure(route::config)
+        .wrap(middleware::Logger::default())
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
+}
